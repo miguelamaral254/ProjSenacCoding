9b573f8933cd32474ee2404a4d75e299c47f4d60
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2024-03-28 20:40:51 -0300

    update sweetAlert2
    a

--- ProjSenacCod/src/Components/Contact.js
+++ ProjSenacCod/src/Components/Contact.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+  const [email, setEmail] = useState("");
+
+  const sweetSuccess = () => {
+    Swal.fire({
+      title: "Enviado!!",
+      text: "Clique aqui para voltar!",
+      icon: "success",
+      confirmButtonClass: "confirmClass",
+      customClass: {
+        title: "titleClass",
+      },
+    });
+    setEmail("");
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  // Verifica se o email é vai ser válido
+  const isEmailValid = () => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
   return (
     <div className="contact-page-wrapper" id="contact">
       <h1 className="primary-heading">
@@ -8,8 +33,19 @@ const Contact = () => {
       </h1>
       <h2>Nos deixe saber!</h2>
       <div className="contact-form-container">
-        <input type="text" placeholder="seuemail@gmail.com" />
-        <button className="secondary-button">Enviar</button>
+        <input
+          type="text"
+          placeholder="seuemail@gmail.com"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <button
+          className="secondary-button"
+          onClick={sweetSuccess}
+          disabled={!isEmailValid()}
+        >
+          Enviar
+        </button>
       </div>
     </div>
   );
